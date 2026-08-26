3f5ffd15836249a7efb68476b4d947e985ce624a
Vasu Kurdia <vasukurdia@gmail.com>
2026-08-26 16:44:22 +0530

    updated code

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -18,7 +18,7 @@ const QuizPage = () => {
       <div className="min-h-screen bg-background">
         <DashboardNavbar />
 
-        <main className="max-w-3xl mx-auto px-6 py-12 flex flex-col items-center gap-6">
+        <main className="w-full px-6 pt-20 pb-12 flex flex-col items-center gap-6">
           <QuizComponent
             title="Javascript Basics Quiz"
             questions={jsBasicsQuestions}
@@ -27,6 +27,7 @@ const QuizPage = () => {
             onQuizSubmit={(finalAnswers) => {
               console.log('Quiz submitted:', finalAnswers);
             }}
+            className="w-full max-w-none"
           />
           <button
             onClick={() => navigate('/resume-screening')}
